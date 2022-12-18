01ff75fcd23640a39bc4d72d4aad884cbc0a1c79
tron01 <69385258+tron01@users.noreply.github.com>
2022-12-18 11:05:57 +0530

    beginPath

--- main.js
+++ main.js
@@ -5,10 +5,19 @@ window.addEventListener("load", () => {
     //Resizing
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
-
-    //ctx.fillRect(40, 100, 200, 400)
-    ctx.strokeStyle = "red";
-    ctx.lineWidth = 5;
-    ctx.strokeRect(40, 100, 200, 400)
-
+    /*
+        ctx.fillRect(60, 50, 100, 400)
+        ctx.strokeStyle = "red";
+        ctx.lineWidth = 5;
+        ctx.strokeRect(40, 100, 200, 400)
+        ctx.strokeStyle = "blue";
+        ctx.lineWidth = 5;
+        ctx.strokeRect(60, 200, 200, 400)
+    */
+    ctx.beginPath();
+    ctx.moveTo(100, 100);
+    ctx.lineTo(200, 100);
+    ctx.lineTo(200, 150);
+    ctx.lineTo(100, 150);
+    ctx.stroke();
 })
