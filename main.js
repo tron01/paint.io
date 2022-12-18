62e1220484945150f6bb36faad13624eb4b93996
tron01 <69385258+tron01@users.noreply.github.com>
2022-12-18 10:53:30 +0530

    init

---
+++ main.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", () => {
+    const canvas = document.querySelector("#canvas")
+    const ctx = canvas.getContext("2d");
+
+    //Resizing
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+
+
+})
